1c3d54406f4f9b8d7466cf00177f3bad6d5b3537
Victor Arnaud <victorhad@gmail.com>
2017-02-18 04:45:07 -0200

    media root

--- vwcurriculum/urls.py
+++ vwcurriculum/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     url(r'^', include('vwcurriculum.core.urls')),
 ]
 
-if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = views.error_404
